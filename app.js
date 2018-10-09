b1111792553b28a9513ad48896fec54852049d6c
Littlenate2114 <littlenate2114@gmail.com>
2018-10-09 21:04:22 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+
+
+//set the template engine ejs
+app.set('view engine', 'ejs')
+
+//middlewares
+app.use(express.static('public'))
+
+
+//routes
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+//Listen on port 3000
+server = app.listen(3000)
+
+
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+	//default username
+	var ran = Math.floor((Math.random() * 100) + 1);
+	var dan = Math.floor((Math.random() * 100) + 1);
+	var fan = Math.floor((Math.random() * 100) + 1);
+	var man = Math.floor((Math.random() * 100) + 1);
+	socket.username = "<span style='color:green'>[user@" + ran + "." + dan +"."+ fan +"."+ man + " ~]$</span>" 
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
