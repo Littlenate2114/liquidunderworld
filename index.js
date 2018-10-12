2398723181a5e15a39ca46a86a877cf4a1cd5aec
Littlenate2114 <littlenate2114@gmail.com>
2018-10-12 11:09:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//-----------------------------------------------------------------------------
+// Configure Express.
+//-----------------------------------------------------------------------------
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -17,32 +20,13 @@ app.get("/", function(req, res) {
     res.render("chat");
 });
 
-//socket.io instantiation
-const io = require("socket.io")(server)
+//-----------------------------------------------------------------------------
+// Configure web sockets.
+//-----------------------------------------------------------------------------
+io.sockets.on("connection", function(socket) {
 
+    socket.on("chat-message", function(message) {
+        io.sockets.emit("chat-message", message);
+    });
 
-//listen on every connection
-io.on('connection', (socket) => {
-	//default username
-	var ran = Math.floor((Math.random() * 100) + 1);
-	var dan = Math.floor((Math.random() * 100) + 1);
-	var fan = Math.floor((Math.random() * 100) + 1);
-	var man = Math.floor((Math.random() * 100) + 1);
-	socket.username = "<span style='color:green'>[user@" + ran + "." + dan +"."+ fan +"."+ man + " ~]$</span>" 
-
-    //listen on change_username
-    socket.on('change_username', (data) => {
-        socket.username = data.username
-    })
-
-    //listen on new_message
-    socket.on('new_message', (data) => {
-        //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
-    })
-
-    //listen on typing
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
-})
+});
